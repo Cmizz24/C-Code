57d79fdb75527854a85c244c8c6212d2954e64a5
cm <claytonmillward22@gmail.com>
2026-06-13 02:40:25 +0100

    Guard context cache stats for task mocks

--- src/core/webview/ClineProvider.ts
+++ src/core/webview/ClineProvider.ts
@@ -71,6 +71,7 @@ import {
 	type MemoryScope,
 	type MemoryState,
 	type MemorySummary,
+	type ContextCacheStats,
 } from "@roo-code/types"
 import {
 	aggregateTaskCostsRecursive,
@@ -171,6 +172,34 @@ function getDetectedContextCacheBudgetOptions() {
 }
 
 const ORCHESTRATOR_MODE_SLUG = "orchestrator"
+
+function getDefaultContextCacheStats(ramBudgetMb: number): ContextCacheStats {
+	return {
+		hotCacheTokens: 0,
+		hotCacheChunks: 0,
+		coldCacheChunks: 0,
+		ramUsedMb: 0,
+		ramBudgetMb,
+		swapsThisSession: 0,
+		condensingAvoided: 0,
+	}
+}
+
+function getTaskContextCacheStats(currentTask: Task | undefined, ramBudgetMb: number): ContextCacheStats {
+	const statsProvider = currentTask as Partial<Pick<Task, "getContextCacheStats">> | undefined
+
+	return typeof statsProvider?.getContextCacheStats === "function"
+		? statsProvider.getContextCacheStats()
+		: getDefaultContextCacheStats(ramBudgetMb)
+}
+
+function getTaskContextCacheWarning(currentTask: Task | undefined): string | undefined {
+	const warningProvider = currentTask as Partial<Pick<Task, "getContextCacheWarning">> | undefined
+
+	return typeof warningProvider?.getContextCacheWarning === "function"
+		? warningProvider.getContextCacheWarning()
+		: undefined
+}
 
 async function restoreDelegatedParentMode(
 	provider: {
@@ -5367,16 +5396,8 @@ export class ClineProvider
 			currentTaskId: currentTask?.taskId,
 			currentTaskItem: currentTask?.taskId ? this.taskHistoryStore.get(currentTask.taskId) : undefined,
 			clineMessages: currentTask?.clineMessages || [],
-			contextCacheStats: currentTask?.getContextCacheStats() ?? {
-				hotCacheTokens: 0,
-				hotCacheChunks: 0,
-				coldCacheChunks: 0,
-				ramUsedMb: 0,
-				ramBudgetMb: normalizedColdCacheRamBudgetMb,
-				swapsThisSession: 0,
-				condensingAvoided: 0,
-			},
-			contextCacheWarning: currentTask?.getContextCacheWarning(),
+			contextCacheStats: getTaskContextCacheStats(currentTask, normalizedColdCacheRamBudgetMb),
+			contextCacheWarning: getTaskContextCacheWarning(currentTask),
 			currentTaskTodos: currentTask?.todoList || [],
 			messageQueue: currentTask?.messageQueueService?.messages,
 			taskHistory: this.taskHistoryStore.getAll().filter((item: HistoryItem) => item.ts && item.task),
